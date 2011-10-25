cb32069c2793dbbaa0065d47c658d3ad31933b45
Bill Jameson <hothousevt@gmail.com>
2011-10-25 19:47:44 -0400

    changed line endings (only affects local repo) crawlURL(): return size fetched and links TODO for next iteration: return word counts

--- hw2/crawler/crawler.py
+++ hw2/crawler/crawler.py
@@ -10,6 +10,22 @@ import re
 from urllib.request import Request, urlopen
 from urllib.error import URLError
 
+def expandURL(URL, topLevel):
+    """Expand URL if it's not an absolute path, using topLevel as the prefix.
+       Returns a string containing the expanded URL."""
+    if re.match('http://|ftp://|www\.', URL) != None:
+        return URL
+    else:
+        r1 = topLevel
+        r2 = URL
+        if topLevel.endswith('/'):
+            r1 = topLevel[:len(topLevel)-1]
+
+        if URL.startswith('/'):
+            r2 = URL[1:]
+        
+        return topLevel + '/' + URL
+
 def parsePage(resp):
     """Parse the page that response resp provides a connection to.
        Arguments: resp is an object of type http.client.HTTPResponse
@@ -17,6 +33,47 @@ def parsePage(resp):
        - the size of the page (in bytes)
        - a list of tuples describing the page's links; each tuple is (address, link text)
        - a dictionary mapping all the words that occur outside of HTML tags to their counts"""
+    # Parse words and links from text documents, while ignoring tags.
+    # Tag-ignoring logic will ignore everything within angle brackets, even in plaintext.
+    contents = resp.read()
+    bytes = len(contents)
+    contents = contents.decode('utf-8')
+
+    # Remove everything in a comment.
+    contents = re.sub('<!--.*?-->', '', contents, 0, re.S)
+
+    # Remove all non-link text.
+    contents = re.sub('(?!</a>)<(?!a ).*?>', '', contents, 0)
+
+    # Parse out the links and link text, then remove link tags.
+    # NB: this pattern does not match links with a missing </a> tag.
+    linkPattern = '<a .*?href="(.*?)".*?>(.*?)</a>'
+    rawLinks = re.findall(linkPattern, contents, re.I)
+    links = []
+    topLevelURL = resp.geturl()
+    for l in rawLinks:
+        fullURL = expandURL(l[0])
+        if fullURL.startswith(topLevelURL):
+            links.append((fullURL, topLevelURL, l[1]))
+    contents = re.sub('<.*?>', '', contents, 0)
+
+    words = {}
+
+    return bytes, links, words
+
+def crawlURL(URL):
+    """Retrieve the web page at URL and parse it for links and words.
+       Return 3 values:
+       - the number of bytes retrieved (-1 if an error is encountered)
+       - a list of tuples containing the links harvested and their corresponding link text
+       - a dictionary containing all the unique words encountered and their word counts"""
+    req = Request(URL)
+    try:
+        resp = urlopen(req)
+    except URLError as e:
+        # Return early if there's an error (including the page requiring authentication).
+        return -1, [], {}
+    
     mimeType = resp.getheader('Content-Type')
     if not mimeType.startswith('text'):
         # Can't easily parse non-text for links/words, so just return the file size.
@@ -27,27 +84,8 @@ def parsePage(resp):
         else:
             return len(resp.read()), [], {}
     else:
-        # Parse words and links from text documents, while ignoring tags.
-        # Tag-ignoring logic will ignore everything within angle brackets, even in plaintext.
-        contents = resp.read().decode('utf-8')
-        # NB: this pattern does not match links with a missing </a> tag.
-        linkPattern = '<a .*?href="(.*?)".*?>(.*?)</a>'
-        links = re.finall(linkPattern, contents)
-
-def crawlURL(URL):
-    """Retrieve the web page at URL and parse it for links and words.
-       Return 3 values:
-       - the number of bytes retrieved (-1 if an error is encountered)
-       - a list of tuples containing the links harvested and their corresponding link text
-       - a dictionary containing all the unique words encountered and their word counts"""
-    req = urllib.request.Request(URL)
-    try:
-        resp = urllib.request.urlopen(req)
         bytes, links, words = parsePage(resp)
         return bytes, links, words
-    except URLError as e:
-        # Return early if there's an error (including the page requiring authentication).
-        return -1, [], {}
 
 def crawlSite(rootURL):
     # List of whistelisted MIME types to recursively crawl (only parse text-based ones)
