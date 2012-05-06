d4ec080d52b5e77ac40bab8dc51140808f012488
Bill Jameson <hothousevt@gmail.com>
2012-05-06 15:47:29 -0400

    99codes Aggregator
    Python program to aggregate code fragments from Notch's XBLA
    Minecraft code giveaway.

---
+++ 99codes/aggregate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+author: Bill Jameson (jamesw2@rpi.edu)
+99codes aggregator: aggregate info on XBLA Minecraft codes different people get from 99codes.mojang.com
+"""
+
+def main():
+    print('aggreaging codes...')
+
+if __name__ == '__main__':
+    main()
+
